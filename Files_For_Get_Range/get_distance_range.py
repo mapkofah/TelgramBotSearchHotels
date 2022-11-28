11ab7527a14d88b09024225cf200c70ab79004b7
Владислав Ковалёв <afganets0@yandex.ru>
2022-11-28 12:46:55 +0500

    Build command bestdeal

---
+++ Files_For_Get_Range/get_distance_range.py
@@ -0,0 +1,46 @@
+from Bot_Commands.help import user_help
+from Bot_Files.my_bot import my_bot
+from Bot_Files.user_class import User
+from Files_For_Get_Hotels.amount_hotels_page import amount_hotels_page
+
+
+def distance_range(message) -> None:
+    chat_id = message.chat.id
+    msg = my_bot.send_message(chat_id, 'Введите расстояние от центра')
+    distance_min(msg)
+
+
+def distance_min(message) -> None:
+    chat_id = message.chat.id
+    msg = my_bot.send_message(chat_id, 'Введите минимальное расстояние')
+    my_bot.register_next_step_handler(msg, check_distance)
+
+
+def distance_max(message) -> None:
+    chat_id = message.chat.id
+    msg = my_bot.send_message(chat_id, 'Введите максимальное расстояние')
+    my_bot.register_next_step_handler(msg, check_distance)
+
+
+def check_distance(message) -> None:
+    chat_id = message.chat.id
+    user = User.get_user(chat_id)
+    try:
+        int(message.text)
+        if message.text == '/help' or message.text == '/start':
+            raise FileNotFoundError
+    except FileNotFoundError:
+        user_help(message)
+    except ValueError:
+        msg = my_bot.send_message(chat_id, 'Неверный ввод, вводите минимальную цену цифрами')
+        if not user.distance_min:
+            distance_min(msg)
+        else:
+            distance_max(msg)
+    else:
+        if not user.distance_min:
+            user.distance_min = message.text
+            distance_max(message)
+        else:
+            user.distance_max = message.text
+            amount_hotels_page(message)
---
+++ Files_For_Get_Range/get_price_range.py
@@ -0,0 +1,46 @@
+from Bot_Commands.help import user_help
+from Bot_Files.my_bot import my_bot
+from Bot_Files.user_class import User
+from Files_For_Get_Range.get_distance_range import distance_range
+
+
+def price_range(message) -> None:
+    chat_id = message.chat.id
+    msg = my_bot.send_message(chat_id, 'Введите диапазон цен')
+    price_min(msg)
+
+
+def price_min(message) -> None:
+    chat_id = message.chat.id
+    msg = my_bot.send_message(chat_id, 'Введите минимальную цену')
+    my_bot.register_next_step_handler(msg, check_price)
+
+
+def price_max(message) -> None:
+    chat_id = message.chat.id
+    msg = my_bot.send_message(chat_id, 'Введите максимальную цену')
+    my_bot.register_next_step_handler(msg, check_price)
+
+
+def check_price(message) -> None:
+    chat_id = message.chat.id
+    user = User.get_user(chat_id)
+    try:
+        int(message.text)
+        if message.text == '/help' or message.text == '/start':
+            raise FileNotFoundError
+    except FileNotFoundError:
+        user_help(message)
+    except ValueError:
+        msg = my_bot.send_message(chat_id, 'Неверный ввод, вводите минимальную цену цифрами')
+        if not user.price_min:
+            price_min(msg)
+        else:
+            price_max(msg)
+    else:
+        if not user.price_min:
+            user.price_min = message.text
+            price_max(message)
+        else:
+            user.price_max = message.text
+            distance_range(message)
